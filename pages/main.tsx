a2bd80976f5b5b460a80ca078aa53ca90316bbea
LFBarreto <lfb.access@gmail.com>
2021-12-14 18:00:02 +0100

    fix

--- pages/main.tsx
+++ pages/main.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import dynamic from "next/dynamic";
 
 import mock from "../src/types/mock";
+import { User } from "../src/types";
 
 const Layout = dynamic(() => import("../src/components/Layout"), {
   ssr: false,
 });
 
-export default function Main(): React.ReactElement {
-  const user = mock.Users[0];
+export default function Main({
+  user = mock.Users[0],
+}: {
+  user: User;
+}): React.ReactElement {
   const rooms = mock.Rooms;
 
   return (
