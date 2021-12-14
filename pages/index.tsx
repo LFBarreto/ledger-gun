caf6e5958ba5b06e479a537c96edc0e9f71e71d0
Alexandre Chabrolin <87011321+achabrolin-ledger@users.noreply.github.com>
2021-12-14 15:33:20 +0100

    cleanup unused code and add handleSignMessage func

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,139 +1,72 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import LedgerLiveApi, {
-  WindowMessageTransport,
-  deserializeTransaction,
-} from "@ledgerhq/live-app-sdk";
+import React, { useEffect, useRef, useState } from "react";
+import LedgerLiveApi, { WindowMessageTransport } from "@ledgerhq/live-app-sdk";
+import type { Account } from "@ledgerhq/live-app-sdk";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { Button } from "@ledgerhq/react-ui";
 
 const DebugApp = (): React.ReactElement => {
   const api = useRef<LedgerLiveApi | null>(null);
-  const [, setLastAnswer] = useState<any>(undefined);
-  const [, setAnswerType] = useState<string>("none");
   const [accounts, setAccounts] = useState<any>([]);
-  const [account, setAccount] = useState<any>(null);
-
-  const execute = useCallback(
-    async (method, payload) => {
-      if (!api.current) {
-        return;
-      }
-
-      let action;
-      switch (method) {
-        case "account.list":
-          action = api.current.listAccounts();
-          break;
-        case "account.request":
-          try {
-            action = api.current.requestAccount(payload);
-          } catch (error) {
-            action = Promise.reject(error);
-          }
-          break;
-        case "account.receive":
-          if (account) {
-            action = api.current.receive(account.id);
-          } else {
-            action = Promise.reject(new Error("No accountId selected"));
-          }
-          break;
-        case "transaction.sign":
-          try {
-            const transaction = deserializeTransaction(payload.transaction);
-            action = api.current.signTransaction(
-              account.id,
-              transaction,
-              payload?.params
-            );
-          } catch (error) {
-            action = Promise.reject(error);
-          }
-          break;
-        case "message.sign":
-          try {
-            // @ts-expect-error to update
-            action = api.current._request("message.sign", {
-              accountId: payload.accountId,
-              message: payload.message,
-              params: payload?.params || {},
-            });
-          } catch (error) {
-            action = Promise.reject(error);
-          }
-          break;
-        case "transaction.broadcast":
-          try {
-            const rawSignedTransaction = payload;
-            action = api.current.broadcastSignedTransaction(
-              account.id,
-              rawSignedTransaction
-            );
-          } catch (error) {
-            action = Promise.reject(error);
-          }
-          break;
-        case "currency.list":
-          try {
-            action = api.current.listCurrencies(payload);
-          } catch (error) {
-            action = Promise.reject(error);
-          }
-          break;
-        default:
-          action = Promise.resolve();
-      }
-
-      try {
-        setAnswerType("pending");
-        setLastAnswer("Waiting...");
-        const result = await action;
-        setAnswerType("success");
-        setLastAnswer(result);
-        if (method === "account.list") {
-          setAccounts(result);
-        }
-      } catch (err: any) {
-        setLastAnswer({ message: err.message });
-        console.error(err);
-        setAnswerType("error");
-      }
-    },
-    [account]
-  );
+  const [selectedAccount, setSelectedAccount] = useState<Account | null>(null);
 
   useEffect(() => {
     const llapi = new LedgerLiveApi(new WindowMessageTransport());
     api.current = llapi;
 
     llapi.connect();
-    execute("account.list", undefined);
+
+    const initAccounts = async () => {
+      const availableAccounts = await llapi.listAccounts();
+      setAccounts(availableAccounts);
+    };
+
+    initAccounts();
+
     return () => {
       api.current = null;
       void llapi.disconnect();
     };
   }, []);
 
+  const handleSignMessage = async (account: Account) => {
+    if (!api.current) {
+      return;
+    }
+
+    try {
+      const res = await api.current.signMessage({
+        accountId: account.id,
+        message: `${account.address}`,
+      });
+
+      // FIXME: this will be the "password", and `account.address` will be the login
+      console.log({ res });
+    } catch (error) {
+      // FIXME: handle error (for example user canceled signature)
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      {accounts &&
-        accounts.map((acc: any) => {
-          return (
-            <div>
-              <Button
-                onClick={() => {
-                  setAccount(acc);
-                  execute("message.sign", {
-                    accountId: acc.id,
-                    message: `${acc.address}`,
-                  });
-                }}
-              >
-                {acc.name}
-              </Button>
-            </div>
-          );
-        })}
+      <div>Selected account: {selectedAccount?.name || "N/A"}</div>
+      <div>
+        {accounts &&
+          accounts.map((acc: any) => {
+            return (
+              <div>
+                <Button
+                  onClick={() => {
+                    setSelectedAccount(acc);
+                    handleSignMessage(acc);
+                  }}
+                >
+                  {acc.name}
+                </Button>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
