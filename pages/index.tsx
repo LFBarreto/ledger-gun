71ad7cf4ff9be6627d2fc593107df6b3fa24eb97
Alexandre Chabrolin <87011321+achabrolin-ledger@users.noreply.github.com>
2021-12-14 10:44:47 +0100

    list user accounts + add sign address

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,54 +6,17 @@ import LedgerLiveApi, {
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { Button } from "@ledgerhq/react-ui";
-/**
-const PAYLOAD_BROADCAST = {
-  accountId: "ACCOUNT_ID",
-  signedTransaction: {
-    operation: {},
-    signature: "SIGNATURE",
-    expirationDate: null,
-  },
-};
 
-
-const ACTIONS = [
-  { value: "account.list", label: "List Accounts" },
-  { value: "account.request", label: "Request Account", usePayload: true },
-  { value: "account.receive", label: "Verify Address", useAccount: true },
-  {
-    value: "transaction.sign",
-    label: "Sign Transaction",
-    useAccount: true,
-    usePayload: true,
-  },
-  {
-    value: "message.sign",
-    label: "Sign Message",
-    useAccount: true,
-    usePayload: true,
-  },
-  {
-    value: "transaction.broadcast",
-    label: "Broadcast Transaction",
-    useAccount: true,
-    usePayload: PAYLOAD_BROADCAST,
-  },
-  { value: "currency.list", label: "List Currencies", usePayload: true },
-];
-
-const prettyJSON = (payload: any) => JSON.stringify(payload, null, 2);
-*/
 const DebugApp = (): React.ReactElement => {
   const api = useRef<LedgerLiveApi | null>(null);
   const { t } = useTranslation("common");
-  const [, setLastAnswer] = useState<any>(undefined);
-  const [, setAnswerType] = useState<string>("none");
-  const [, setAccounts] = useState<any>([]);
+  const [lastAnswer, setLastAnswer] = useState<any>(undefined);
+  const [answerType, setAnswerType] = useState<string>("none");
+  const [accounts, setAccounts] = useState<any>([]);
   const [account, setAccount] = useState<any>(null);
 
   const execute = useCallback(
-    async (method, rawPayload) => {
+    async (method, payload) => {
       if (!api.current) {
         return;
       }
@@ -65,7 +28,6 @@ const DebugApp = (): React.ReactElement => {
           break;
         case "account.request":
           try {
-            const payload = rawPayload ? JSON.parse(rawPayload) : undefined;
             action = api.current.requestAccount(payload);
           } catch (error) {
             action = Promise.reject(error);
@@ -80,7 +42,6 @@ const DebugApp = (): React.ReactElement => {
           break;
         case "transaction.sign":
           try {
-            const payload = JSON.parse(rawPayload);
             const transaction = deserializeTransaction(payload.transaction);
             action = api.current.signTransaction(
               account.id,
@@ -93,7 +54,6 @@ const DebugApp = (): React.ReactElement => {
           break;
         case "message.sign":
           try {
-            const payload = JSON.parse(rawPayload);
             // @ts-expect-error to update
             action = api.current._request("message.sign", {
               accountId: account.id,
@@ -106,7 +66,7 @@ const DebugApp = (): React.ReactElement => {
           break;
         case "transaction.broadcast":
           try {
-            const rawSignedTransaction = JSON.parse(rawPayload);
+            const rawSignedTransaction = payload;
             action = api.current.broadcastSignedTransaction(
               account.id,
               rawSignedTransaction
@@ -117,7 +77,6 @@ const DebugApp = (): React.ReactElement => {
           break;
         case "currency.list":
           try {
-            const payload = rawPayload ? JSON.parse(rawPayload) : undefined;
             action = api.current.listCurrencies(payload);
           } catch (error) {
             action = Promise.reject(error);
@@ -133,11 +92,8 @@ const DebugApp = (): React.ReactElement => {
         const result = await action;
         setAnswerType("success");
         setLastAnswer(result);
-        if (method.value === "account.list") {
+        if (method === "account.list") {
           setAccounts(result);
-          if (result instanceof Array && result.length) {
-            setAccount(result[0]);
-          }
         }
       } catch (err: any) {
         setLastAnswer({ message: err.message });
@@ -160,7 +116,25 @@ const DebugApp = (): React.ReactElement => {
     };
   }, []);
 
-  return <Button variant="color">{t("hello")}</Button>;
+  return (
+    <div>
+      {accounts &&
+        accounts.map((acc: any) => {
+          return (
+            <div>
+              <Button
+                onClick={() => {
+                  setAccount(acc);
+                  execute("message.sign", { message: `${acc.address}` });
+                }}
+              >
+                {acc.name}
+              </Button>
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 
 export async function getStaticProps({ locale }: { locale: string }) {
